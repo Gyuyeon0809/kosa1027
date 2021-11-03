91aff755c0dae95c207f31f66897cd82124c0383
Gyuyeon0809 <88140403+Gyuyeon0809@users.noreply.github.com>
2021-11-03 17:20:44 +0900

    uyfyug

---
+++ test/src/test/Hello1.java
@@ -0,0 +1,13 @@
+package test;
+
+
+public class Hello1 {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		int i = 10;
+		String a = "Hello";
+		System.out.println(a+i);
+		
+		 
+	}
+}
