5748b6fd7be5d37878d3020b1140ace8e51b993d
Gyuyeon0809 <88140403+Gyuyeon0809@users.noreply.github.com>
2021-11-10 16:26:21 +0900

    2021110

---
+++ 20211110/src/Ex02.java
@@ -0,0 +1,26 @@
+public class Ex02 {
+
+	public static void main(String[] args) {
+		int i1 = 10;
+		int i2 = 20;
+		int i3 = 30;
+		int [] i = new int[3];
+		i[0] = 10; // i1 = 10;
+		i[1] = 20;
+		i[2] = 30;
+		
+		Account ac1 = new Account();
+		Account ac2 = new Account();
+		Account ac3 =  new Account();
+		
+		Account [] accs = new Account[3];
+		accs[0] = new Account("1111","ÀÌ¼þ¹«", 100);
+		accs[1] = new Account();
+		
+		String [] str = new String[3];
+		str[0] = "ÀÌ¼þ¹«";
+		str[1] = "ÀÌÀå¹ü";
+
+	}
+
+}
